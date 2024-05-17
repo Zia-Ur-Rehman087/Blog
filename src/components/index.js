c5395b97665cb31c1b3fa673fd7e0f4a44eb74a7
Zia-Ur-Rehman087 <ziyasoomro087@gmail.com>
2024-05-17 11:09:51 +0500

    s

--- src/components/index.js
+++ src/components/index.js
@@ -12,7 +12,7 @@ import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
-import { Bloge } from "./bloge";
+import { Bloge } from "./Bloge";
 export {
     Header,
     Footer,
